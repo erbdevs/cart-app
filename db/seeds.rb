9829ffe96dad6a3060d871a46b5cbcfecdd2e15a
erbdevs <erb.devs@gmail.com>
2023-10-05 20:38:05 +0200

    Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+[
+	{
+    code: 'GR1',
+    name: 'Green Tea',
+    price: 3.11
+  },
+	{
+    code: 'SR1',
+    name: 'Strawberries',
+    price: 5.00
+  },
+	{
+    code: 'CF1',
+    name: 'Cofee',
+    price: 11.23
+  }
+].each do |product_hash|
+  Product.find_or_create_by(product_hash)
+end
