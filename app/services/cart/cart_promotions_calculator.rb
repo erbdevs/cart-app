6568d39bbaf5d5f19f21a3eb4fb47f8f875088b1
erbdevs <erb.devs@gmail.com>
2023-10-25 20:42:16 +0200

    Little refactor for PromotionsCalculator

--- app/services/cart/cart_promotions_calculator.rb
+++ app/services/cart/cart_promotions_calculator.rb
@@ -1,31 +1,38 @@
 class Cart
   class CartPromotionsCalculator
     def self.run(cart)
-      cart.cart_items.each do |cart_item|
-        promotion = Promotion.find_by(product_id: cart_item.product_id)
-        next unless promotion
+      delete_stale_promotions(cart)
+      generate_new_promotions(cart)
+    end
 
-        if cart_item.quantity < promotion.min_quantity
-          cart_promotion = cart.cart_promotions.find_by(promotion_id: promotion.id)
-          cart_promotion.destroy if cart_promotion
-        else
-          cart_promotion = cart.cart_promotions.find_or_initialize_by(promotion_id: promotion.id)
-          items_out_of_promotion = cart_item.quantity % promotion.bulk_quantity
-          unitary_discount = promotion.product.price * promotion.percentage / 100
-          cart_promotion.value = unitary_discount * (cart_item.quantity - items_out_of_promotion)
-          cart_promotion.save
-        end
-      end
+  private
 
+    def self.delete_stale_promotions(cart)
       cart.cart_promotions.each do |cart_promotion|
         cart_promotion.destroy if stale_cart_promotion?(cart_promotion)
       end
     end
 
-  private
-
     def self.stale_cart_promotion?(cart_promotion)
-      !CartItem.exists?(cart_id: cart_promotion.cart_id, product_id: cart_promotion.promotion.product_id)
+      cart_item = CartItem.find_by(cart_id: cart_promotion.cart_id, product_id: cart_promotion.promotion.product_id)
+      return true unless cart_item
+      return true if cart_item.quantity < cart_promotion.promotion.min_quantity
+
+      false
+    end
+
+    def self.generate_new_promotions(cart)
+      Promotion.where(product_id: cart.cart_items.pluck(:product_id)).each do |promotion|
+        cart_item = cart.cart_items.find_by(product_id: promotion.product_id)
+        next if cart_item.quantity < promotion.min_quantity
+
+        cart_promotion = cart.cart_promotions.find_or_initialize_by(promotion_id: promotion.id)
+        items_out_of_promotion = cart_item.quantity % promotion.bulk_quantity
+        unitary_discount = promotion.product.price * promotion.percentage / 100
+        total_discount = unitary_discount * (cart_item.quantity - items_out_of_promotion)
+
+        cart_promotion.update!(value: total_discount)
+      end
     end
   end
 end
