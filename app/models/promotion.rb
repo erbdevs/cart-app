256f9b5ec43af67c025df9634193514dc267c9e4
erbdevs <erb.devs@gmail.com>
2023-10-26 00:25:47 +0200

    Some validation for Promotion model

--- app/models/promotion.rb
+++ app/models/promotion.rb
@@ -1,3 +1,13 @@
 class Promotion < ApplicationRecord
   belongs_to :product
+
+  validate :validate_min_quantity_and_bulk_quantity
+
+private
+
+  def validate_min_quantity_and_bulk_quantity
+    return if bulk_quantity <= min_quantity
+
+    errors.add("bulk_quantity must be smaller than min_quantity")
+  end
 end
--- spec/models/promotion_spec.rb
+++ spec/models/promotion_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Promotion, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:product) { create(:product) }
+  let(:promotion_instance) { build(:promotion, product: product) }
+
+  it 'is valid' do
+    byebug
+    expect(promotion_instance.valid?).to eq(true)
+  end
+
+  it 'is invalid if bulk_quantity is greater than min_quantity' do
+    promotion_instance.min_quantity = 3
+    promotion_instance.bulk_quantity = 4
+
+    expect(promotion_instance.valid?).to eq(false)
+  end
 end
